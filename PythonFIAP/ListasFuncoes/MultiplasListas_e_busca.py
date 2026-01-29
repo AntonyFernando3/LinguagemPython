bd851d9d33451e3dba1cf4fce74a01becba5a354
Antony Fernando <89105412+AntonyFernando3@users.noreply.github.com>
2026-01-29 16:33:11 -0300

    Add files via upload

---
+++ PythonFIAP/ListasFuncoes/MultiplasListas_e_busca.py
@@ -0,0 +1,20 @@
+equipamentos = []
+valores = []
+seriais = []
+departamentos = []
+resposta = "S"
+while resposta == "S":
+    print("\n ....preencha as informações a baixo.... \n" )
+    equipamentos.append(input("Equipamento: "))
+    valores.append(float(input("Valor: ")))
+    seriais.append(int(input("Número Serial: ")))
+    departamentos.append(input("Departamento: "))
+    resposta = input("Digite \"S\" para continuar: ").upper()
+
+busca=input("\nDigite o nome do equipamento que deseja buscar: ")
+for indice in range(0,len(equipamentos)):
+ if busca==equipamentos[indice]:
+    print("Nome.........: ", equipamentos[indice])
+    print("Valor........: ", valores[indice])
+    print("Serial.......: ", seriais[indice])
+    print("Departamento.: ", departamentos[indice])
---
+++ PythonFIAP/ListasFuncoes/Variaveis_e_listas.py
@@ -0,0 +1,13 @@
+#inventário
+inventario=[]
+resposta="S"
+while resposta=="S":
+    inventario.append(input("Equipamento: "))
+    inventario.append(float(input("Valor: ")))
+    inventario.append(int(input("Número Serial: ")))
+    inventario.append(input("Departamento: "))
+    resposta=input("Digite \"S\" para continuar: ").upper()
+
+#ativando os codigos abaixo ele faz a impressão das informações
+#for elemento in inventario:
+ #print(elemento)
